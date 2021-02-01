04f4a0b9fbc41a555203d4a6ce05bbcf239ae852
Filip Sądowicz <sadowicz31@gmail.com>
2021-02-01 20:21:09 +0100

    Adding parsing port number

---
+++ monochord/parse.c
@@ -0,0 +1,14 @@
+#include "parse.h"
+
+short strToPort(char* str)
+{
+    char* endptr = NULL;
+    errno = 0;
+
+    long res = strtol(str, &endptr, 10);
+
+    if(errno || *endptr || res < 0 || res > 32767)
+        errExit("strToPort: Unable to convert string to port number");
+
+    return (short)res;
+}
---
+++ monochord/parse.h
@@ -0,0 +1,12 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "utils.h"
+
+short strToPort(char* str);
+
+#endif
