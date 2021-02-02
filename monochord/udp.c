54669615a4c39b47cc8cc5c3896e7e0b754cb519
Filip Sądowicz <sadowicz31@gmail.com>
2021-02-02 11:09:02 +0100

    Adding socket initialization functions to udp

--- monochord/udp.c
+++ monochord/udp.c
@@ -1,5 +1,25 @@
-//
-// Created by sadowicz on 01.02.21.
-//
-
 #include "udp.h"
+
+int initSocket(short port)
+{
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd == -1)
+        errExit("initSocket: Unable to create socket file descriptor");
+
+    registerSocket(sockfd, port);
+
+    return sockfd;
+}
+
+void registerSocket(int sockfd, short port)
+{
+    struct sockaddr_in sAddr = {
+            .sin_family = AF_INET,
+            .sin_port = htons((in_port_t)port),
+            .sin_addr.s_addr = htonl(INADDR_ANY)    // htonl probably not necessary since INADDR_ANY is 0,
+                                                    // but placed here for cleaner code
+    };
+
+    if(bind(sockfd, (struct sockaddr*)&sAddr, sizeof(sAddr)))
+        errExit("registerSocket: Unable to bind socket");
+}
--- monochord/udp.h
+++ monochord/udp.h
@@ -1,6 +1,14 @@
 #ifndef UDP_H
 #define UDP_H
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+#include "utils.h"
+
+int initSocket(short port);
+void registerSocket(int sockfd, short port);
 
 #endif
