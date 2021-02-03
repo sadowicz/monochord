d0792ce78dc31524796f13141e1b85d869df075e
Filip Sądowicz <sadowicz31@gmail.com>
2021-02-03 21:52:57 +0100

    Adding timestamp writing functions to recorder

--- recorder/utils.c
+++ recorder/utils.c
@@ -28,3 +28,61 @@ void registerSignalHandler(int signal, void (*handler)(int, siginfo_t*, void *))
     if(sigaction(signal, &sa, NULL))
         errExit("registerSignalHandler: Unable to register signal handler");
 }
+
+void writeTimestampGlobal(int fd)
+{
+    struct timespec ts;
+
+    if(clock_gettime(CLOCK_REALTIME, &ts))
+        errExit("writeTimestampGlobal: Unable to get clock value");
+
+    struct tm readable;
+
+    if(!localtime_r(&(ts.tv_sec), &readable))
+        errExit("writeTimestampGlobal: Unable to get human readable time values");
+
+    char buf[MAX_TIMESTAMP_LEN] = {0};
+
+    long milliseconds = ts.tv_nsec / 1000000;
+
+    sprintf(buf, "%d:%d:%d.%ld",
+            readable.tm_hour, readable.tm_min, readable.tm_sec, milliseconds);
+
+    if(write(fd, buf, strlen(buf)) == -1)
+        errExit("writeTimestampGlobal: Unable to write timestamp to file");
+}
+
+void writeTimestampLocal(int fd, struct timespec* refPoint)
+{
+    struct timespec diff;
+    struct timespec curr;
+
+    if(clock_gettime(CLOCK_MONOTONIC, &curr))
+        errExit("writeTimestampLocal: Unable to get clock value");
+
+    getTimestampDiff(&diff, refPoint, &curr);
+
+    time_t hours = diff.tv_sec / 3600;
+    time_t mins = (diff.tv_sec % 3600) / 60;
+    time_t sec = ((diff.tv_sec % 3600) % 60) / 60;
+    long milliseconds = diff.tv_nsec / 1000000;
+
+    char buf[MAX_TIMESTAMP_LEN] = {0};
+
+    sprintf(buf, "%ld:%ld:%ld.%ld",
+            hours, mins, sec, milliseconds);
+
+    if(write(fd, buf, strlen(buf)) == -1)
+        errExit("writeTimestampLocal: Unable to write timestamp to file");
+}
+
+void getTimestampDiff(struct timespec* res, struct timespec* earlier, struct timespec* later)
+{
+    res->tv_sec = later->tv_sec - earlier->tv_sec;
+
+    if(later->tv_nsec < earlier->tv_nsec)
+    {
+        res->tv_sec--;
+        res->tv_nsec = 1000000000 - earlier->tv_nsec + later->tv_nsec;
+    }
+}
--- recorder/utils.h
+++ recorder/utils.h
@@ -2,14 +2,25 @@
 #define UTILS_H
 
 #define _POSIX_C_SOURCE 199309L
+#define _BSD_SOURCE
+#define _SVID_SOURCE
+
+#define MAX_TIMESTAMP_LEN 32
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
 
 void errExit(const char* msg);
 void usageExit(const char* programName, const char* msg);
 
 void registerSignalHandler(int signal, void (*handler)(int, siginfo_t*, void *));
+
+void writeTimestampGlobal(int fd);
+void writeTimestampLocal(int fd, struct timespec* refPoint);
+void getTimestampDiff(struct timespec* res, struct timespec* earlier, struct timespec* later);
 
 #endif
