de5f250157d0bc9fb52bf57781f4d46ee472b284
Filip Sądowicz <sadowicz31@gmail.com>
2021-02-06 11:36:51 +0100

    Adding prompting and str-converting functions to rejestrator_info utils

--- info_rejestrator/utils.c
+++ info_rejestrator/utils.c
@@ -1,1 +1,35 @@
 #include "utils.h"
+
+void errExit(const char* msg)
+{
+    fprintf(stderr, "ERROR: %s\n", msg);
+    exit(1);
+}
+
+void usageExit(const char* programName, const char* msg)
+{
+    fprintf(stderr, "USAGE: %s %s\n", programName, msg);
+    exit(1);
+}
+
+int strToInt(char* str)
+{
+    char* endptr = NULL;
+    errno = 0;
+
+    long res = strtol(str, &endptr, 10);
+
+    if(errno || *endptr)
+        errExit("strToInt: Unable to convert string to int number");
+
+    return (int)res;
+}
+
+int strToSig(char* str)
+{
+    int sig = strToInt(str);
+    if(sig <= 0 || sig > SIGRTMAX)
+        errExit("strToSig: Signal number out of range");    // not specified if program must accept only rt signals
+
+    return sig;
+}
--- info_rejestrator/utils.h
+++ info_rejestrator/utils.h
@@ -1,4 +1,14 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
+
+void errExit(const char* msg);
+void usageExit(const char* programName, const char* msg);
+
+
+
 #endif
